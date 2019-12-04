470a6f53b11172b2dd681615d3772e5f980ca7ac
Suman-Exalogic <suman@exalogic.in>
2019-12-04 17:50:01 +0530

    6 Commit

--- db/migrate/20191204072938_add_gsal_to_users.rb
+++ db/migrate/20191204072938_add_gsal_to_users.rb
@@ -1,6 +1,16 @@
 class AddGsalToUsers < ActiveRecord::Migration[6.0]
   def change
-  	add_column :users,:gsal,:decimal
+  	add_column :users,:bp,:decimal
+  	add_column :users,:hra,:decimal
+  	add_column :users,:cca,:decimal
+  	add_column :users,:sa,:decimal
+  	add_column :users,:ta,:decimal
+  	add_column :users,:reim,:decimal
+  	add_column :users,:it,:decimal
+  	add_column :users,:pt,:decimal
+  	add_column :users,:lop,:decimal
+  	add_column :users,:od,:decimal
+	add_column :users,:gsal,:decimal
   	add_column :users,:nol,:decimal
   	add_column :users,:ns,:decimal 
   end
