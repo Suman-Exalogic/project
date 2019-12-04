dbd8b23cc5288b3d996d9ef9bfa52e5d4737bef7
Suman-Exalogic <suman@exalogic.in>
2019-12-04 18:04:16 +0530

    initial commit

--- db/migrate/20191204072938_add_gsal_to_users.rb
+++ db/migrate/20191204072938_add_gsal_to_users.rb
@@ -1,17 +1,17 @@
 class AddGsalToUsers < ActiveRecord::Migration[6.0]
   def change
-  	add_column :users,:bp,:decimal
-  	add_column :users,:hra,:decimal
-  	add_column :users,:cca,:decimal
-  	add_column :users,:sa,:decimal
-  	add_column :users,:ta,:decimal
-  	add_column :users,:reim,:decimal
-  	add_column :users,:it,:decimal
-  	add_column :users,:pt,:decimal
-  	add_column :users,:lop,:decimal
-  	add_column :users,:od,:decimal
-	add_column :users,:gsal,:decimal
-  	add_column :users,:nol,:decimal
-  	add_column :users,:ns,:decimal 
+  	add_column :users,:bp,:decimal, :default=>0
+  	add_column :users,:hra,:decimal, :default=>0
+  	add_column :users,:cca,:decimal, :default=>0
+  	add_column :users,:sa,:decimal, :default=>0
+  	add_column :users,:ta,:decimal, :default=>0
+  	add_column :users,:reim,:decimal, :default=>0
+  	add_column :users,:it,:decimal, :default=>0
+  	add_column :users,:pt,:decimal, :default=>0
+  	add_column :users,:lop,:decimal, :default=>0
+  	add_column :users,:od,:decimal, :default=>0
+	add_column :users,:gsal,:decimal, :default=>0
+  	add_column :users,:nol,:decimal, :default=>0
+  	add_column :users,:ns,:decimal, :default=>0
   end
 end
