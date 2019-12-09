762e7a903a6ffc6d356ed04ad8003bf24a191e6c
Suman-Exalogic <suman@exalogic.in>
2019-12-09 17:47:03 +0530

    3 Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_123305) do
+ActiveRecord::Schema.define(version: 2019_12_06_100050) do
+
+  create_table "charges", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "fname"
@@ -35,9 +40,23 @@ ActiveRecord::Schema.define(version: 2019_11_26_123305) do
     t.date "doj"
     t.string "department"
     t.string "designation"
-    t.integer "empid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "bp", default: "0.0"
+    t.decimal "hra", default: "0.0"
+    t.decimal "cca", default: "0.0"
+    t.decimal "sa", default: "0.0"
+    t.decimal "ta", default: "0.0"
+    t.decimal "reim", default: "0.0"
+    t.decimal "it", default: "0.0"
+    t.decimal "pt", default: "0.0"
+    t.decimal "lop", default: "0.0"
+    t.decimal "od", default: "0.0"
+    t.decimal "gsal", default: "0.0"
+    t.decimal "nol", default: "0.0"
+    t.decimal "ns", default: "0.0"
+    t.integer "empid"
+    t.integer "bacc"
   end
 
 end
